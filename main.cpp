5726d2224c274c5045405bad904858193c3356da
DimitriiTrater <dima1603040@gmail.com>
2023-01-06 10:22:05 +0200

    Add collision bal Y in func

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 #include "player/player.hpp"
 #include "ball/ball.hpp"
 
+
+
+void collisionBallY(float& dy, Ball& ball, int HEIGHT)
+{
+    if ((ball.getY() + ball.getR() > HEIGHT) || (ball.getY() - ball.getR() < 0))
+        dy = -dy;
+}
+
+
+
 int main(int argc, char* argv[])
 {
     const int WIDTH = 800; // window width
@@ -48,8 +58,6 @@ int main(int argc, char* argv[])
     winText.setFillColor(white);
     winText.setPosition(330.f, 200.f);
     
-
-
 
     // ball create
     Ball ball(400.f, 300.f, 20.f);
@@ -146,8 +154,7 @@ int main(int argc, char* argv[])
     	    ball.setY(ball.getY()+dy);
         }
         
-        if ((ball.getY() + ball.getR() > HEIGHT) || (ball.getY() - ball.getR() < 0))
-            dy = -dy;
+        collisionBallY(dy, ball, HEIGHT);
         
         if ((ball.getX() + ball.getR() <= (firstPlayer.getX()+50)) && ((firstPlayer.getY() <= (ball.getY() + ball.getR())) && ((ball.getY() + ball.getR()) <= (firstPlayer.getY() + 200.f))))
     	{   
